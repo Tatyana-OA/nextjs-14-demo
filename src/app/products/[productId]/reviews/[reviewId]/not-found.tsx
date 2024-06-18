e72a2dc1be76c8fe687eebd06fe937820097a3fe
TAsenova <tasenova@mentormate.com>
2024-06-18 18:25:46 +0300

    Catch all Segments in dynamic routing, Not found pages

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReviewNotFound = () => {
+  return <div>Cannot have more than 1000 reviews.</div>;
+};
+
+export default ReviewNotFound;
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { notFound } from "next/navigation";
 
 const ReviewDetail = ({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) => {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <div>
       Review {params.reviewId} for Product {params.productId}
