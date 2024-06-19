6f2673b5bfabec33e0b7992ffdc477b520c9c0f0
TAsenova <tasenova@mentormate.com>
2024-06-19 15:09:18 +0300

    Error Boundary

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { notFound } from "next/navigation";
 
+const getRandomInt = (count: number) => {
+  return Math.floor(Math.random() * count);
+};
+
 const ReviewDetail = ({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) => {
+  // Simulating Error
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error Loading Review");
+  }
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
