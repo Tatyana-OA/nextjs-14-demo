bd3e7e674ded7c5262bda09e0e2bf647b9847a68
TAsenova <tasenova@mentormate.com>
2024-06-19 15:11:31 +0300

    Recover from error

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -2,8 +2,19 @@
 
 import React from "react";
 
-const ErrorBoundary = ({ error }: { error: Error }) => {
-  return <div>{error.message}</div>;
+const ErrorBoundary = ({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) => {
+  return (
+    <>
+      <button onClick={reset}> Try Again </button>
+      <div>{error.message}</div>
+    </>
+  );
 };
 
 export default ErrorBoundary;
