0e10cc7103e81027c6754feb5e53cf60c0533b3a
TAsenova <tasenova@mentormate.com>
2024-06-20 16:01:25 +0300

    Route handler

---
+++ src/app/hello/api/route.tsx
@@ -0,0 +1,7 @@
+//Route Handler
+export async function GET() {
+  return new Response("Hello World");
+}
+
+//hello -> will show page
+//hello/api -> will show route handler
---
+++ src/app/hello/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// If both page + route are present, the route will get handled by default
+// Page won't show
+const HelloPage = () => {
+  return <div>HelloPage</div>;
+};
+
+export default HelloPage;
