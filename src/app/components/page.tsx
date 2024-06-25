e6c65fb56667d09a2b85e7eefd372a622a1931d4
TAsenova <tasenova@mentormate.com>
2024-06-25 15:52:50 +0300

    Using third party packages in client vs server routes

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,9 +1,6 @@
 import { serverSideFunction } from "../../utils/server-utils";
 
-// In a server route, we can use a client functionality (slider) ONLY if encapsulated
-
 import ImageSlider from "../components/ImageSlider";
-
 const ServerPage = () => {
   const result = serverSideFunction();
   return (
